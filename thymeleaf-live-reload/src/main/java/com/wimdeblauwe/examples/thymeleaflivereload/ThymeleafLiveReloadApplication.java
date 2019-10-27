ae9ebd5dcd1f3824351acb3068913fcf0ad86331
Wim Deblauwe <wim.deblauwe@pegus.digital>
2019-10-27 13:27:50 +0100

    Add example code for thymeleaf live reload blog post

---
+++ thymeleaf-live-reload/src/main/java/com/wimdeblauwe/examples/thymeleaflivereload/ThymeleafLiveReloadApplication.java
@@ -0,0 +1,13 @@
+package com.wimdeblauwe.examples.thymeleaflivereload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafLiveReloadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafLiveReloadApplication.class, args);
+	}
+
+}
