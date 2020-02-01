9ee4e80a5123cd26370fed7e5a275a8bba3739d1
Wim Deblauwe <wim.deblauwe@pegus.digital>
2020-02-01 14:53:01 +0100

    Initial commit testcontainers-cypress-example

---
+++ testcontainers-cypress-example/src/test/java/com/wimdeblauwe/examples/testcontainerscypressexample/TestcontainersCypressExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wimdeblauwe.examples.testcontainerscypressexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestcontainersCypressExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
