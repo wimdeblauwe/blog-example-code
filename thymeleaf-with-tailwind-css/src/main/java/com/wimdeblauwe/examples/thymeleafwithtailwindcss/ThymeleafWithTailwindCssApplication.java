40096d37962cf11f2ff9ab9d8df2ec39fbf71ab3
Wim Deblauwe <wim.deblauwe@pegus.digital>
2020-02-13 16:29:27 +0100

    Generated project from start.spring.io

---
+++ thymeleaf-with-tailwind-css/src/main/java/com/wimdeblauwe/examples/thymeleafwithtailwindcss/ThymeleafWithTailwindCssApplication.java
@@ -0,0 +1,13 @@
+package com.wimdeblauwe.examples.thymeleafwithtailwindcss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafWithTailwindCssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafWithTailwindCssApplication.class, args);
+	}
+
+}
---
+++ thymeleaf-with-tailwind-css/src/test/java/com/wimdeblauwe/examples/thymeleafwithtailwindcss/ThymeleafWithTailwindCssApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wimdeblauwe.examples.thymeleafwithtailwindcss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafWithTailwindCssApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
