af7ffdd2bcba860f5f1263d50c2a5d2a4c6cfc9a
Wim Deblauwe <wim.deblauwe@pegus.digital>
2020-02-17 21:59:34 +0100

    Project generated from start.spring.io

---
+++ laravel-intermediate-task-list/src/test/java/com/wimdeblauwe/examples/laravelintermediatetasklist/LaravelIntermediateTaskListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wimdeblauwe.examples.laravelintermediatetasklist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaravelIntermediateTaskListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
