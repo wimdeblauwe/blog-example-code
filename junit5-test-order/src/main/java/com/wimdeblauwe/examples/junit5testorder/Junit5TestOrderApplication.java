98afaafda1532c264ca9f489f6c459003554a2a4
Wim Deblauwe <wim.deblauwe@gmail.com>
2021-02-12 15:36:11 +0100

    Initial commit from start.spring.io

---
+++ junit5-test-order/src/main/java/com/wimdeblauwe/examples/junit5testorder/Junit5TestOrderApplication.java
@@ -0,0 +1,13 @@
+package com.wimdeblauwe.examples.junit5testorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Junit5TestOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Junit5TestOrderApplication.class, args);
+	}
+
+}
---
+++ junit5-test-order/src/test/java/com/wimdeblauwe/examples/junit5testorder/Junit5TestOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wimdeblauwe.examples.junit5testorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Junit5TestOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
