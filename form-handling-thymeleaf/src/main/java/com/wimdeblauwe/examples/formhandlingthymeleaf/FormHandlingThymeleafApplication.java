d2975603dac5c97ed886eb197ee4d52dabad3ed2
Wim Deblauwe <wim.deblauwe@gmail.com>
2021-05-23 11:21:38 +0200

    Form handling thymeleaf: initial commit

---
+++ form-handling-thymeleaf/src/main/java/com/wimdeblauwe/examples/formhandlingthymeleaf/FormHandlingThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.wimdeblauwe.examples.formhandlingthymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormHandlingThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormHandlingThymeleafApplication.class, args);
+	}
+
+}
---
+++ form-handling-thymeleaf/src/test/java/com/wimdeblauwe/examples/formhandlingthymeleaf/FormHandlingThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wimdeblauwe.examples.formhandlingthymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormHandlingThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
