0beda0bab871d4c990c33ec2fd2e3a26b315968c
Wim Deblauwe <wim.deblauwe@gmail.com>
2021-07-25 14:32:06 +0200

    Thymeleaf with records: initial commit

---
+++ thymeleaf-with-records/src/main/java/com/wimdeblauwe/examples/thymeleafwithrecords/ThymeleafWithRecordsApplication.java
@@ -0,0 +1,13 @@
+package com.wimdeblauwe.examples.thymeleafwithrecords;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafWithRecordsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafWithRecordsApplication.class, args);
+	}
+
+}
---
+++ thymeleaf-with-records/src/test/java/com/wimdeblauwe/examples/thymeleafwithrecords/ThymeleafWithRecordsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wimdeblauwe.examples.thymeleafwithrecords;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafWithRecordsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
