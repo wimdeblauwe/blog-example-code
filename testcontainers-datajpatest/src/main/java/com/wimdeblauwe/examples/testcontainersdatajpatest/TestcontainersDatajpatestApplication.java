e8a7c414265d36a3353b074b283b502fae56c629
Wim Deblauwe <wim.deblauwe@gmail.com>
2021-07-30 17:24:33 +0200

    Testcontainers-DataJpaTest: Initial commit

---
+++ testcontainers-datajpatest/src/main/java/com/wimdeblauwe/examples/testcontainersdatajpatest/TestcontainersDatajpatestApplication.java
@@ -0,0 +1,13 @@
+package com.wimdeblauwe.examples.testcontainersdatajpatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestcontainersDatajpatestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestcontainersDatajpatestApplication.class, args);
+	}
+
+}
---
+++ testcontainers-datajpatest/src/test/java/com/wimdeblauwe/examples/testcontainersdatajpatest/TestcontainersDatajpatestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wimdeblauwe.examples.testcontainersdatajpatest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestcontainersDatajpatestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
