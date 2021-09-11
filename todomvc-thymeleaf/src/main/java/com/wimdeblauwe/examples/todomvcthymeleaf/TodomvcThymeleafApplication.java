d584778b67e3259c618b02179ab524ef05b16c7a
Wim Deblauwe <wim.deblauwe@gmail.com>
2021-09-11 13:26:16 +0200

    todomvc-thymeleaf: Initial commit

---
+++ todomvc-thymeleaf/src/main/java/com/wimdeblauwe/examples/todomvcthymeleaf/TodomvcThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.wimdeblauwe.examples.todomvcthymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodomvcThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodomvcThymeleafApplication.class, args);
+	}
+
+}
---
+++ todomvc-thymeleaf/src/test/java/com/wimdeblauwe/examples/todomvcthymeleaf/TodomvcThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wimdeblauwe.examples.todomvcthymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodomvcThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
