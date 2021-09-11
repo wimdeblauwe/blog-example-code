cefc3e2a14b9d3b8104bfbf7cdd326c4c4886f29
Wim Deblauwe <wim.deblauwe@gmail.com>
2021-09-11 15:36:50 +0200

    todomvc-thymeleaf: Add a new todo item

--- todomvc-thymeleaf/src/main/java/com/wimdeblauwe/examples/todomvcthymeleaf/todoitem/web/TodoItemController.java
+++ todomvc-thymeleaf/src/main/java/com/wimdeblauwe/examples/todomvcthymeleaf/todoitem/web/TodoItemController.java
@@ -1,10 +1,15 @@
 package com.wimdeblauwe.examples.todomvcthymeleaf.todoitem.web;
 
+import com.wimdeblauwe.examples.todomvcthymeleaf.todoitem.TodoItem;
 import com.wimdeblauwe.examples.todomvcthymeleaf.todoitem.TodoItemRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/")
@@ -18,7 +23,15 @@ public class TodoItemController {
 
     @GetMapping
     public String index(Model model) {
+        model.addAttribute("item", new TodoItemFormData());
         model.addAttribute("totalNumberOfItems", repository.count());
         return "index";
+    }
+
+    @PostMapping
+    public String addNewTodoItem(@Valid @ModelAttribute("item") TodoItemFormData formData) {
+        repository.save(new TodoItem(formData.getTitle(), false));
+
+        return "redirect:/";
     }
 }
