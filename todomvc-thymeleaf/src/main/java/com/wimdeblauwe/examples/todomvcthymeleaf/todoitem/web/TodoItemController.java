2b9290473da0b3b08f1b916b1a3b0f81232d7027
Wim Deblauwe <wim.deblauwe@gmail.com>
2021-09-11 15:25:30 +0200

    todomvc-thymeleaf: Hide sections if there are no todos

--- todomvc-thymeleaf/src/main/java/com/wimdeblauwe/examples/todomvcthymeleaf/todoitem/web/TodoItemController.java
+++ todomvc-thymeleaf/src/main/java/com/wimdeblauwe/examples/todomvcthymeleaf/todoitem/web/TodoItemController.java
@@ -1,6 +1,8 @@
 package com.wimdeblauwe.examples.todomvcthymeleaf.todoitem.web;
 
+import com.wimdeblauwe.examples.todomvcthymeleaf.todoitem.TodoItemRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,8 +10,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/")
 public class TodoItemController {
 
+    private final TodoItemRepository repository;
+
+    public TodoItemController(TodoItemRepository repository) {
+        this.repository = repository;
+    }
+
     @GetMapping
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("totalNumberOfItems", repository.count());
         return "index";
     }
 }
