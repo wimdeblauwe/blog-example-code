599a00787c646b77159371c147a24c1f93a2c7d0
Wim Deblauwe <wim.deblauwe@gmail.com>
2021-09-11 13:31:32 +0200

    todomvc-thymeleaf: Add entity and repository

---
+++ todomvc-thymeleaf/src/main/java/com/wimdeblauwe/examples/todomvcthymeleaf/todoitem/TodoItemRepository.java
@@ -0,0 +1,6 @@
+package com.wimdeblauwe.examples.todomvcthymeleaf.todoitem;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TodoItemRepository extends JpaRepository<TodoItem, Long> {
+}
