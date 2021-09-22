fece17c9b7364ade29276eeaa498cf349467f360
Wim Deblauwe <wim.deblauwe@gmail.com>
2021-09-22 15:45:53 +0200

    todomvc-htmx: Initial commit

---
+++ todomvc-htmx/src/main/java/com/wimdeblauwe/examples/todomvchtmx/TodomvcThymeleafHtmxApplication.java
@@ -0,0 +1,13 @@
+package com.wimdeblauwe.examples.todomvchtmx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodomvcThymeleafHtmxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodomvcThymeleafHtmxApplication.class, args);
+	}
+
+}
