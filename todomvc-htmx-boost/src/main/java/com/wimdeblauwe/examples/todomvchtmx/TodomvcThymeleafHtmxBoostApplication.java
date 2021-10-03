3436aa0dfc057af74dfdf8c5dce0a993f8e4f743
Wim Deblauwe <wim.deblauwe@gmail.com>
2021-10-03 14:16:38 +0200

    todomvc-htmx-boost: Initial commit

---
+++ todomvc-htmx-boost/src/main/java/com/wimdeblauwe/examples/todomvchtmx/TodomvcThymeleafHtmxBoostApplication.java
@@ -0,0 +1,13 @@
+package com.wimdeblauwe.examples.todomvchtmx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodomvcThymeleafHtmxBoostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodomvcThymeleafHtmxBoostApplication.class, args);
+	}
+
+}
