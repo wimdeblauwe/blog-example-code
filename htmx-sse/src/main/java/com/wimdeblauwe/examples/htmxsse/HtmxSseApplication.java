e1a4a77e88258b236fc618fd8e37bb151cb093ce
Wim Deblauwe <wim.deblauwe@gmail.com>
2021-10-10 14:03:36 +0200

    htmx-sse: Initial commit

---
+++ htmx-sse/src/main/java/com/wimdeblauwe/examples/htmxsse/HtmxSseApplication.java
@@ -0,0 +1,13 @@
+package com.wimdeblauwe.examples.htmxsse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HtmxSseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HtmxSseApplication.class, args);
+	}
+
+}
---
+++ htmx-sse/src/test/java/com/wimdeblauwe/examples/htmxsse/HtmxSseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wimdeblauwe.examples.htmxsse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HtmxSseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
