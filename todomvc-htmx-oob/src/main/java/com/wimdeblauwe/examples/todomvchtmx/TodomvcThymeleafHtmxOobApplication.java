3862d29bde5ac5fd07771bcb747eddd5f6c26d4d
Wim Deblauwe <wim.deblauwe@gmail.com>
2022-05-18 20:32:17 +0200

    todomvc htmx: add alternative with OOB swaps

---
+++ todomvc-htmx-oob/src/main/java/com/wimdeblauwe/examples/todomvchtmx/TodomvcThymeleafHtmxOobApplication.java
@@ -0,0 +1,13 @@
+package com.wimdeblauwe.examples.todomvchtmx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodomvcThymeleafHtmxOobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodomvcThymeleafHtmxOobApplication.class, args);
+	}
+
+}
---
+++ todomvc-htmx-oob/src/main/java/com/wimdeblauwe/examples/todomvchtmx/todoitem/web/TodoItemController.java
@@ -0,0 +1,186 @@
+package com.wimdeblauwe.examples.todomvchtmx.todoitem.web;
+
+import com.wimdeblauwe.examples.todomvchtmx.todoitem.TodoItem;
+import com.wimdeblauwe.examples.todomvchtmx.todoitem.TodoItemNotFoundException;
+import com.wimdeblauwe.examples.todomvchtmx.todoitem.TodoItemRepository;
+import io.github.wimdeblauwe.hsbt.mvc.HtmxResponse;
+import io.github.wimdeblauwe.hsbt.mvc.HxRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Controller
+@RequestMapping("/")
+public class TodoItemController {
+
+    private final TodoItemRepository repository;
+
+    public TodoItemController(TodoItemRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping
+    public String index(Model model) {
+        addAttributesForIndex(model, ListFilter.ALL);
+        return "index";
+    }
+
+    @GetMapping("/active")
+    public String indexActive(Model model) {
+        addAttributesForIndex(model, ListFilter.ACTIVE);
+        return "index";
+    }
+
+    @GetMapping("/completed")
+    public String indexCompleted(Model model) {
+        addAttributesForIndex(model, ListFilter.COMPLETED);
+        return "index";
+    }
+
+    @PostMapping
+    public String addNewTodoItem(@Valid @ModelAttribute("item") TodoItemFormData formData) {
+        repository.save(new TodoItem(formData.getTitle(), false));
+
+        return "redirect:/";
+    }
+
+    @PutMapping("/{id}/toggle")
+    public String toggleSelection(@PathVariable("id") Long id) {
+        TodoItem todoItem = repository.findById(id)
+                                      .orElseThrow(() -> new TodoItemNotFoundException(id));
+
+        todoItem.setCompleted(!todoItem.isCompleted());
+        repository.save(todoItem);
+        return "redirect:/";
+    }
+
+    @PutMapping("/toggle-all")
+    public String toggleAll() {
+        List<TodoItem> todoItems = repository.findAll();
+        for (TodoItem todoItem : todoItems) {
+            todoItem.setCompleted(!todoItem.isCompleted());
+            repository.save(todoItem);
+        }
+        return "redirect:/";
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteTodoItem(@PathVariable("id") Long id) {
+        repository.deleteById(id);
+
+        return "redirect:/";
+    }
+
+    @DeleteMapping("/completed")
+    public String deleteCompletedItems() {
+        List<TodoItem> items = repository.findAllByCompleted(true);
+        for (TodoItem item : items) {
+            repository.deleteById(item.getId());
+        }
+        return "redirect:/";
+    }
+
+    @PostMapping
+    @HxRequest
+    public HtmxResponse htmxAddTodoItem(TodoItemFormData formData,
+                                        Model model) {
+        TodoItem item = repository.save(new TodoItem(formData.getTitle(), false));
+        return getHtmxResponseForTodoItem(model, item);
+    }
+
+    @PutMapping("/{id}/toggle")
+    @HxRequest
+    public HtmxResponse htmxToggleTodoItem(@PathVariable("id") Long id,
+                                           Model model) {
+        TodoItem todoItem = repository.findById(id)
+                                      .orElseThrow(() -> new TodoItemNotFoundException(id));
+
+        todoItem.setCompleted(!todoItem.isCompleted());
+        repository.save(todoItem);
+
+        return getHtmxResponseForTodoItem(model, todoItem);
+    }
+
+    @DeleteMapping("/{id}")
+    @HxRequest
+    public HtmxResponse htmxDeleteTodoItem(@PathVariable("id") Long id,
+                                           Model model) {
+        repository.deleteById(id);
+
+        return activeItemsCountFragment(model);
+    }
+
+    @GetMapping("/active-items-count")
+    @HxRequest
+    public String htmxActiveItemsCount(Model model) {
+        model.addAttribute("numberOfActiveItems", getNumberOfActiveItems());
+
+        return "fragments :: active-items-count";
+    }
+
+    private HtmxResponse getHtmxResponseForTodoItem(Model model,
+                                                    TodoItem item) {
+        model.addAttribute("item", toDto(item));
+        return new HtmxResponse()
+                .addTemplate("fragments :: todoItem")
+                .and(activeItemsCountFragment(model));
+    }
+
+    private HtmxResponse activeItemsCountFragment(Model model) {
+        model.addAttribute("numberOfActiveItems", getNumberOfActiveItems());
+        return new HtmxResponse()
+                .addTemplate("fragments :: active-items-count");
+    }
+
+    private void addAttributesForIndex(Model model,
+                                       ListFilter listFilter) {
+        model.addAttribute("item", new TodoItemFormData());
+        model.addAttribute("filter", listFilter);
+        model.addAttribute("todos", getTodoItems(listFilter));
+        model.addAttribute("totalNumberOfItems", repository.count());
+        model.addAttribute("numberOfActiveItems", getNumberOfActiveItems());
+        model.addAttribute("numberOfCompletedItems", getNumberOfCompletedItems());
+    }
+
+    private List<TodoItemDto> getTodoItems(ListFilter filter) {
+        return switch (filter) {
+            case ALL -> convertToDto(repository.findAll());
+            case ACTIVE -> convertToDto(repository.findAllByCompleted(false));
+            case COMPLETED -> convertToDto(repository.findAllByCompleted(true));
+        };
+    }
+
+    private List<TodoItemDto> convertToDto(List<TodoItem> todoItems) {
+        return todoItems
+                .stream()
+                .map(this::toDto)
+                .collect(Collectors.toList());
+    }
+
+    private TodoItemDto toDto(TodoItem todoItem) {
+        return new TodoItemDto(todoItem.getId(),
+                               todoItem.getTitle(),
+                               todoItem.isCompleted());
+    }
+
+    private int getNumberOfActiveItems() {
+        return repository.countAllByCompleted(false);
+    }
+
+    private int getNumberOfCompletedItems() {
+        return repository.countAllByCompleted(true);
+    }
+
+    public static record TodoItemDto(long id, String title, boolean completed) {
+    }
+
+    public enum ListFilter {
+        ALL,
+        ACTIVE,
+        COMPLETED
+    }
+}
