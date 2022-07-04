e55e18e900cc5cd9aaf3af67d35b751350bd4d88
Wim Deblauwe <wim.deblauwe@gmail.com>
2022-07-04 20:37:48 +0200

    live reload dev tools: init Spring Boot project

---
+++ live-reload-dev-tools/src/main/java/com/wimdeblauwe/examples/livereloaddevtools/LiveReloadDevToolsApplication.java
@@ -0,0 +1,13 @@
+package com.wimdeblauwe.examples.livereloaddevtools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveReloadDevToolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiveReloadDevToolsApplication.class, args);
+	}
+
+}
---
+++ live-reload-dev-tools/src/test/java/com/wimdeblauwe/examples/livereloaddevtools/LiveReloadDevToolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wimdeblauwe.examples.livereloaddevtools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveReloadDevToolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
