b7e554ed54a13397a3b165cac15aa7dbe28018ed
Wim Deblauwe <wim.deblauwe@gmail.com>
2022-09-30 18:24:27 +0200

    Thymeleaf htmx auth error handling: Initial commit

---
+++ thymeleaf-htmx-auth-error-handling/src/main/java/com/wimdeblauwe/examples/thymeleafhtmxautherrorhandling/ThymeleafHtmxAuthErrorHandlingApplication.java
@@ -0,0 +1,13 @@
+package com.wimdeblauwe.examples.thymeleafhtmxautherrorhandling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafHtmxAuthErrorHandlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafHtmxAuthErrorHandlingApplication.class, args);
+	}
+
+}
