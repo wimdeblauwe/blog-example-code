074d8513a62b3c67edaf2dbdd386948b94382e54
Wim Deblauwe <wim.deblauwe@gmail.com>
2023-02-18 18:04:10 +0100

    Shoelace Thymeleaf demo: Add project skeleton using Tailwind, Alpine and htmx

---
+++ shoelace-thymeleaf/src/main/java/com/deblauwe/examples/shoelacethymeleaf/ShoelaceThymeleafDemoApplication.java
@@ -0,0 +1,13 @@
+package com.deblauwe.examples.shoelacethymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoelaceThymeleafDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoelaceThymeleafDemoApplication.class, args);
+	}
+
+}
