da0243d12a1997a0d8eac4805e20705f2f2b2df8
Wim Deblauwe <wim.deblauwe@gmail.com>
2023-03-05 09:35:54 +0100

    ViewComponent demo: init project

---
+++ viewcomponent-demo/src/main/java/com/wimdeblauwe/examples/viewcomponentdemo/ViewComponentDemoApplication.java
@@ -0,0 +1,13 @@
+package com.wimdeblauwe.examples.viewcomponentdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViewComponentDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViewComponentDemoApplication.class, args);
+	}
+
+}
