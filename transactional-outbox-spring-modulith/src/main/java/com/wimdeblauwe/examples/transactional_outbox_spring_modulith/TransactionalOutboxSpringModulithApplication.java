dff64987259c2a4f0ae6cffa518a7b837dcd0cc9
Wim Deblauwe <wim.deblauwe@gmail.com>
2024-06-13 07:32:40 +0200

    Transactional Outbox: Init Spring Modulith implementation

---
+++ transactional-outbox-spring-modulith/src/main/java/com/wimdeblauwe/examples/transactional_outbox_spring_modulith/TransactionalOutboxSpringModulithApplication.java
@@ -0,0 +1,13 @@
+package com.wimdeblauwe.examples.transactional_outbox_spring_modulith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionalOutboxSpringModulithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionalOutboxSpringModulithApplication.class, args);
+	}
+
+}
---
+++ transactional-outbox-spring-modulith/src/test/java/com/wimdeblauwe/examples/transactional_outbox_spring_modulith/TransactionalOutboxSpringModulithApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wimdeblauwe.examples.transactional_outbox_spring_modulith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionalOutboxSpringModulithApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
