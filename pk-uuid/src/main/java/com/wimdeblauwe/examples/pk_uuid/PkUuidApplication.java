febf670cffe50906abe78a520c50a885e49b2a3f
Wim Deblauwe <wim.deblauwe@gmail.com>
2024-09-18 08:21:26 +0200

    pk-uuid: generated project setup

---
+++ pk-uuid/src/main/java/com/wimdeblauwe/examples/pk_uuid/PkUuidApplication.java
@@ -0,0 +1,13 @@
+package com.wimdeblauwe.examples.pk_uuid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PkUuidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PkUuidApplication.class, args);
+	}
+
+}
---
+++ pk-uuid/src/test/java/com/wimdeblauwe/examples/pk_uuid/PkUuidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wimdeblauwe.examples.pk_uuid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PkUuidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
