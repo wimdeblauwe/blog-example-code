7e14b6bf5881409b08acf96bd7824914e6aae0d5
Wim Deblauwe <wim.deblauwe@gmail.com>
2024-11-17 21:16:36 +0100

    Redirect attributes: init

---
+++ redirect-attributes/src/main/java/com/wimdeblauwe/examples/redirect_attributes/RedirectAttributesDemoApplication.java
@@ -0,0 +1,13 @@
+package com.wimdeblauwe.examples.redirect_attributes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedirectAttributesDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedirectAttributesDemoApplication.class, args);
+	}
+
+}
---
+++ redirect-attributes/src/test/java/com/wimdeblauwe/examples/redirect_attributes/RedirectAttributesDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wimdeblauwe.examples.redirect_attributes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedirectAttributesDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
