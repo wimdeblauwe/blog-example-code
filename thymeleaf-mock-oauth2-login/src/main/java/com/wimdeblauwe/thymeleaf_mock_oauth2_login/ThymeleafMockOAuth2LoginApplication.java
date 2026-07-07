fb8839fcd366e8484f7b8c7c171b3b305ada8190
Wim Deblauwe <wim.deblauwe@gmail.com>
2026-07-07 09:52:29 +0200

    mock-oauth2-server: init project

---
+++ thymeleaf-mock-oauth2-login/src/main/java/com/wimdeblauwe/thymeleaf_mock_oauth2_login/ThymeleafMockOAuth2LoginApplication.java
@@ -0,0 +1,13 @@
+package com.wimdeblauwe.thymeleaf_mock_oauth2_login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafMockOAuth2LoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafMockOAuth2LoginApplication.class, args);
+	}
+
+}
---
+++ thymeleaf-mock-oauth2-login/src/test/java/com/wimdeblauwe/thymeleaf_mock_oauth2_login/ThymeleafMockOAuth2LoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wimdeblauwe.thymeleaf_mock_oauth2_login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafMockOAuth2LoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
